59968181ec4612bb655950bc508e4b9c58a9b129
Abner Yang <abtyang@gmail.com>
2017-08-01 16:56:00 -0400

    changed variable and function names to camel case

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-var Board;
-var xsize = 10;
-var ysize = 10;
+var board;
+var xSize = 10;
+var ySize = 10;
 
 var dead = 0;
 var alive = 1;
 
-function Neighbors(Board, x, y) {
+function neighbors(board, x, y) {
 	var n = 0
 	for (var dx = -1; dx < 1; ++dx) {
 		for (var dy = -1; dy < 1; ++dy) {
 			var ax = x + dx;
 			var ay = y + dy;
-			if (Board[ax][ay] == alive) {
+			if (board[ax][ay] == alive) {
 				++n;
 			}
 		}
@@ -19,85 +19,85 @@ function Neighbors(Board, x, y) {
 	return n;
 }
 
-function Kill(Board, x, y) {
-	if (Board[x][y] == alive) {
-		Board[x][y] = dead;
+function kill(board, x, y) {
+	if (board[x][y] == alive) {
+		board[x][y] = dead;
 	}
 }
 
-function MakeLive(Board, x, y) {
-	if (Board[x][y] == dead) {
-		Board[x][y] = alive;
+function makeLive(board, x, y) {
+	if (board[x][y] == dead) {
+		board[x][y] = alive;
 	}
 }
 
-function NextStep(Board) {
-	for (var x = 0; x <= xsize; ++x) {
-		for (var y = 0; y <= ysize; ++x) {
-			var n = Neighbors(Board, x, y);
+function nextStep(board) {
+	for (var x = 0; x <= xSize; ++x) {
+		for (var y = 0; y <= ySize; ++x) {
+			var n = neighbors(board, x, y);
 			if (n = 3) {
-				MakeLive(Board, x, y);
+				makeLive(board, x, y);
 			}
 			if ((n < 2) || (n > 3)) {
-				Kill(Board, x, y);
+				kill(board, x, y);
 			}
 		}
 	}
 }
 
-function DrawBoard(Board) {
+function drawBoard(board) {
 	var Text = "";
-	for (var y = 0; y < ysize; ++y) {
-		for (var x = 0; x < xsize; ++x) {
-			Text += Board[x][y] == alive ? "o" : "_";
+	for (var y = 0; y < ySize; ++y) {
+		for (var x = 0; x < xSize; ++x) {
+			Text += board[x][y] == alive ? "o" : "_";
 		}
 		Text += "<br/>";
 	}
 	document.getElementById("board").innerHTML = Text;
 }
 
-function Main() {
+function main() {
     // *** Change this variable to choose a different baord setup from below
-    var BoardSetup = "blinker";
+    var boardSetup = "blinker";
     
-	Board = new Array(xsize);
-	for (var x = 0; x < xsize; ++x) {
-		Board[x] = new Array(ysize);
-		for (var y = 0; y < ysize; ++y) {
-			Board[x][y] = 0;
+	board = new Array(xSize);
+	for (var x = 0; x < xSize; ++x) {
+		board[x] = new Array(ySize);
+		for (var y = 0; y < ySize; ++y) {
+			board[x][y] = 0;
 		}
 	}
 	
-	if (BoardSetup == "blinker") {
-	    Board[1][0] = 1;
-	    Board[1][1] = 1;
-	    Board[1][2] = 1;
-    } else if(BoardSetup == "glider") {
-	    Board[2][0] = 1;
-	    Board[2][1] = 1;
-	    Board[2][2] = 1;
-	    Board[1][2] = 1;
-	    Board[0][1] = 1;
-    } else if(BoardSetup == "flower") {
-        Board[4][6] = 1;
-        Board[5][6] = 1;
-        Board[6][6] = 1;
-        Board[7][6] = 1;
-        Board[8][6] = 1;
-        Board[9][6] = 1;
-        Board[10][6] = 1;
-        Board[4][7] = 1;
-        Board[6][7] = 1;
-        Board[8][7] = 1;
-        Board[10][7] = 1;
-        Board[4][8] = 1;
-        Board[5][8] = 1;
-        Board[6][8] = 1;
-        Board[7][8] = 1;
-        Board[8][8] = 1;
-        Board[9][8] = 1;
-        Board[10][8] = 1;
+	if (boardSetup == "blinker") {
+	    board[1][0] = 1;
+	    board[1][1] = 1;
+	    board[1][2] = 1;
+    } else if(boardSetup == "glider") {
+	    board[2][0] = 1;
+	    board[2][1] = 1;
+	    board[2][2] = 1;
+	    board[1][2] = 1;
+	    board[0][1] = 1;
+    } else if(boardSetup == "flower") {
+        board[4][6] = 1;
+        board[5][6] = 1;
+        board[6][6] = 1;
+        board[7][6] = 1;
+        board[8][6] = 1;
+        board[9][6] = 1;
+        board[10][6] = 1;
+        board[4][7] = 1;
+        board[6][7] = 1;
+        board[8][7] = 1;
+        board[10][7] = 1;
+        board[4][8] = 1;
+        board[5][8] = 1;
+        board[6][8] = 1;
+        board[7][8] = 1;
+        board[8][8] = 1;
+        board[9][8] = 1;
+        board[10][8] = 1;
     }
     
-	DrawBoard(Board);
+	drawBoard(board);
 }
